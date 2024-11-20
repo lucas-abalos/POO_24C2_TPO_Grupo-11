f15fcf68a1ea805adf8266ec6baed01b3c84c819
PatricioPlem <pplem@uade.edu.ar>
2024-11-20 18:06:07 -0300

    diagramas mejora heroe y reclamo recompensas

--- El Desafío del Reino Encantado/src/reino/main/Main.java
+++ El Desafío del Reino Encantado/src/reino/main/Main.java
@@ -1,7 +1,7 @@
 package reino.main;
 
 import reino.controller.JuegoController;
-
+import reino.*;
 public class Main {
     public static void main(String[] args) {
         JuegoController.iniciarJuego();
